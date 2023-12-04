79c66a74eb129872c5808a9f9979868f3bfabb08
Kevin Simpson <ktsimpso@gmail.com>
2023-12-03 22:02:19 -0800

    Completed day 4 part 2

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,21 +1,11 @@
 use crate::libs::{
     cli::{CliProblem, Command},
-    graph::BoundedPoint,
-    parse::{parse_digit, parse_lines, parse_table, parse_usize, StringParse},
+    parse::{parse_lines, parse_usize, StringParse},
     problem::Problem,
 };
-use chumsky::{
-    error::Rich,
-    extra,
-    primitive::{choice, just},
-    IterParser, Parser,
-};
+use chumsky::{error::Rich, extra, primitive::just, IterParser, Parser};
 use clap::{Args, ValueEnum};
-use itertools::Itertools;
-use std::{
-    cell::LazyCell,
-    collections::{BTreeSet, HashMap},
-};
+use std::{cell::LazyCell, collections::HashMap};
 
 pub const DAY_04: LazyCell<Box<dyn Command>> = LazyCell::new(|| {
     Box::new(
@@ -26,8 +16,16 @@ pub const DAY_04: LazyCell<Box<dyn Command>> = LazyCell::new(|| {
         )
         .with_part(
             "Finds the score for each scratch card and sums the values.",
-            CommandLineArguments {},
-        ), //.with_part("help", CommandLineArguments {}),
+            CommandLineArguments {
+                rules: GameRules::Score,
+            },
+        )
+        .with_part(
+            "Finds the total number of scratch cards won.",
+            CommandLineArguments {
+                rules: GameRules::Cards,
+            },
+        ),
     )
 });
 
@@ -41,21 +39,14 @@ struct Card {
 
 impl StringParse for Input {
     fn parse<'a>() -> impl Parser<'a, &'a str, Self, extra::Err<Rich<'a, char>>> {
+        let spaces = just(" ").repeated().at_least(1);
         let card = just("Card")
-            .then_ignore(just(" ").repeated().at_least(1))
+            .then_ignore(spaces)
             .ignore_then(parse_usize())
-            .then_ignore(just(": ").then(just(" ").or_not()))
-            .then(
-                parse_usize()
-                    .separated_by(just(" ").repeated().at_least(1))
-                    .collect::<Vec<_>>(),
-            )
-            .then_ignore(just(" |").then_ignore(just(" ").repeated().at_least(1)))
-            .then(
-                parse_usize()
-                    .separated_by(just(" ").repeated().at_least(1))
-                    .collect::<Vec<_>>(),
-            )
+            .then_ignore(just(":").then(spaces))
+            .then(parse_usize().separated_by(spaces).collect::<Vec<_>>())
+            .then_ignore(just(" |").then_ignore(spaces))
+            .then(parse_usize().separated_by(spaces).collect::<Vec<_>>())
             .map(|((number, winning_numbers), match_numbers)| Card {
                 number,
                 winning_numbers,
@@ -65,8 +56,17 @@ impl StringParse for Input {
     }
 }
 
+#[derive(ValueEnum, Clone)]
+enum GameRules {
+    Score,
+    Cards,
+}
+
 #[derive(Args)]
-struct CommandLineArguments {}
+struct CommandLineArguments {
+    #[arg(short, long, help = "The game rules to use")]
+    rules: GameRules,
+}
 
 struct Day04 {}
 
@@ -74,8 +74,31 @@ impl Problem<Input, CommandLineArguments> for Day04 {
     type Output = usize;
 
     fn run(input: Input, arguments: &CommandLineArguments) -> Self::Output {
-        input.0.into_iter().map(|card| count_points(&card)).sum()
+        match arguments.rules {
+            GameRules::Score => input.0.into_iter().map(|card| count_points(&card)).sum(),
+            GameRules::Cards => {
+                let mut card_map = HashMap::new();
+                input.0.into_iter().for_each(|card| {
+                    let matches = count_matches(&card);
+                    let current_card_count = card_map.entry(card.number).or_insert(0usize);
+                    *current_card_count += 1;
+                    let current_card_count = *current_card_count;
+
+                    for i in 1..=matches {
+                        *card_map.entry(card.number + i).or_insert(0usize) += current_card_count;
+                    }
+                });
+                card_map.values().sum()
+            }
+        }
     }
+}
+
+fn count_matches(card: &Card) -> usize {
+    card.match_numbers
+        .iter()
+        .filter(|number| card.winning_numbers.contains(number))
+        .count()
 }
 
 fn count_points(card: &Card) -> usize {
