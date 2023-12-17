392c23549b10c05dc95f8346e6129486a5f4fd1d
Kevin Simpson <ktsimpso@gmail.com>
2023-12-16 19:28:10 -0800

    Cleaned up day9

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,25 +1,31 @@
 use crate::libs::{
     cli::{CliProblem, Command},
-    parse::{parse_isize, parse_lines, parse_usize, StringParse},
+    parse::{parse_isize, parse_lines, StringParse},
     problem::Problem,
 };
-use chumsky::{
-    error::Rich,
-    extra,
-    primitive::{end, just},
-    text::newline,
-    IterParser, Parser,
-};
+use chumsky::{error::Rich, extra, primitive::just, IterParser, Parser};
 use clap::Args;
-use integer_sqrt::IntegerSquareRoot;
-use std::cell::LazyCell;
+use std::{
+    cell::LazyCell,
+    ops::{Add, Sub},
+};
 use tap::Tap;
 
 pub const DAY_09: LazyCell<Box<dyn Command>> = LazyCell::new(|| {
     Box::new(
-        CliProblem::<Input, CommandLineArguments, Day09>::new("day09", "help", "file help")
-            .with_part("part1", CommandLineArguments {})
-            .with_part("part2", CommandLineArguments {}),
+        CliProblem::<Input, CommandLineArguments, Day09>::new(
+            "day09",
+            "Finds the next value in sequences and sums them",
+            "One sequence per line each value separated by a space.",
+        )
+        .with_part(
+            "Calculates the next value at the back",
+            CommandLineArguments { previous: false },
+        )
+        .with_part(
+            "Calculates the previous value at the front",
+            CommandLineArguments { previous: true },
+        ),
     )
 });
 
@@ -32,7 +38,14 @@ impl StringParse for Input {
 }
 
 #[derive(Args)]
-struct CommandLineArguments {}
+struct CommandLineArguments {
+    #[arg(
+        short,
+        long,
+        help = "If the value at the front should be calculated instead of the back"
+    )]
+    previous: bool,
+}
 
 struct Day09 {}
 
@@ -40,26 +53,39 @@ impl Problem<Input, CommandLineArguments> for Day09 {
     type Output = isize;
 
     fn run(input: Input, arguments: &CommandLineArguments) -> Self::Output {
+        let push = if arguments.previous {
+            |last_items: &mut Vec<isize>, sequence: &Vec<isize>| {
+                last_items.push(*sequence.first().expect("At least one item"))
+            }
+        } else {
+            |last_items: &mut Vec<isize>, sequence: &Vec<isize>| {
+                last_items.push(*sequence.last().expect("At least one item"))
+            }
+        };
+        let operation: fn(isize, isize) -> isize = if arguments.previous {
+            isize::sub
+        } else {
+            isize::add
+        };
+
         input
             .0
             .into_iter()
             .map(|sequence| {
                 let mut last_items = Vec::new();
-                //last_items.push(*sequence.last().expect("At least one item"));
-                last_items.push(*sequence.first().expect("At least one item"));
+                push(&mut last_items, &sequence);
 
                 let mut next_sequence = sequence;
 
                 while !next_sequence.iter().all(|value| value == &0) {
                     next_sequence = map_sequence(&next_sequence);
-                    //last_items.push(*next_sequence.last().expect("At least one item"))
-                    last_items.push(*next_sequence.first().expect("At least one item"))
+                    push(&mut last_items, &next_sequence);
                 }
 
                 last_items
                     .tap_mut(|items| items.reverse())
                     .into_iter()
-                    .fold(0, |acc, value| value - acc)
+                    .fold(0, |acc, value| operation(value, acc))
             })
             .sum()
     }
