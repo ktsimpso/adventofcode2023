61adf6a567935af973ea1b9cde3e5d0dc115314f
Kevin Simpson <ktsimpso@gmail.com>
2023-12-16 19:27:21 -0800

    Cleaned up day 8

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,29 +1,35 @@
 use crate::libs::{
     cli::{CliProblem, Command},
-    graph::BoundedPoint,
-    parse::{parse_digit, parse_lines, parse_table, parse_usize, StringParse},
+    parse::{parse_lines, StringParse},
     problem::Problem,
 };
 use chumsky::{
     error::Rich,
     extra,
-    primitive::{any, choice, end, just},
+    primitive::{any, just},
     text::newline,
     IterParser, Parser,
 };
-use clap::{Args, ValueEnum};
-use itertools::Itertools;
-use std::{
-    cell::LazyCell,
-    collections::{BTreeMap, BTreeSet, HashMap},
-};
+use clap::Args;
+use num_integer::Integer;
+use std::{cell::LazyCell, collections::BTreeMap};
 use tap::Tap;
 
 pub const DAY_08: LazyCell<Box<dyn Command>> = LazyCell::new(|| {
     Box::new(
-        CliProblem::<Input, CommandLineArguments, Day08>::new("day08", "help", "file help")
-            .with_part("part1", CommandLineArguments {})
-            .with_part("part2", CommandLineArguments {}),
+        CliProblem::<Input, CommandLineArguments, Day08>::new(
+            "day08",
+            "Finds the route from the start to the finish",
+            "The left/right steps to take followed by mappings between nodes.",
+        )
+        .with_part(
+            "Finds the number of steps from AAA to the finsh",
+            CommandLineArguments { all: false },
+        )
+        .with_part(
+            "Finds the number of steps from all A nodes that would finish at the same time.",
+            CommandLineArguments { all: true },
+        ),
     )
 });
 
@@ -75,7 +81,10 @@ impl StringParse for Input {
 }
 
 #[derive(Args)]
-struct CommandLineArguments {}
+struct CommandLineArguments {
+    #[arg(short, long, help = "Whether to find all items with A or just AAA")]
+    all: bool,
+}
 
 struct Day08 {}
 
@@ -84,16 +93,20 @@ impl Problem<Input, CommandLineArguments> for Day08 {
 
     fn run(input: Input, arguments: &CommandLineArguments) -> Self::Output {
         let directions = input.directions.into_iter().cycle();
-        let mut current = input
-            .map
-            .keys()
-            .filter(|key| key.ends_with("A"))
-            .cloned()
-            .collect::<Vec<_>>();
+        let mut current = if arguments.all {
+            input
+                .map
+                .keys()
+                .filter(|key| key.ends_with("A"))
+                .cloned()
+                .collect::<Vec<_>>()
+        } else {
+            vec!["AAA".to_string()]
+        };
+
         let initial = current.clone();
-        println!("Started with: {} nodes", initial.len());
         let mut found = initial.iter().map(|_| false).collect::<Vec<_>>();
-        //let mut current = "AAA".to_string();
+        let mut found_step = Vec::new();
         let mut steps = 0usize;
 
         for direction in directions {
@@ -107,16 +120,15 @@ impl Problem<Input, CommandLineArguments> for Day08 {
                     }
                 })
                 .collect::<Vec<_>>();
-            //println!("================");
             steps += 1;
             initial
                 .iter()
                 .zip(current.iter())
                 .zip(found.iter_mut())
-                .for_each(|((i, c), been_found)| {
+                .for_each(|((_, c), been_found)| {
                     if c.ends_with("Z") && !*been_found {
                         *been_found = true;
-                        println!("I: {} loops after: {}", i, steps);
+                        found_step.push(steps);
                     }
                 });
             if found.iter().all(|c| *c) {
@@ -126,18 +138,8 @@ impl Problem<Input, CommandLineArguments> for Day08 {
             if current.iter().all(|c| c.ends_with("Z")) {
                 break;
             }
-            //println!("------------------");
-            //let mapping = input.map.get(&current).expect("Mapping exists");
-            /*match direction {
-                Direction::Left => current = mapping.0.clone(),
-                Direction::Right => current = mapping.1.clone(),
-            };
-            steps += 1;
-            if current == "ZZZ".to_string() {
-                break;
-            }*/
         }
 
-        steps
+        found_step.into_iter().fold(1, |acc, step| acc.lcm(&step))
     }
 }
